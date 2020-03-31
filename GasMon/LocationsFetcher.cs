827026d9a36738e7e48e99457b24383c99c0d525
Jamie Baird <james.baird@softwire.com>
2020-03-31 11:18:46 +0100

    Function now returns a list of type Location which deserialises the json

---
+++ GasMon/LocationsFetcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json.Serialization;
+using Amazon.S3;
+using Newtonsoft.Json;
+
+namespace GasMon
+{
+    public class LocationsFetcher
+    {
+        private readonly IAmazonS3 client;
+
+
+        public LocationsFetcher(IAmazonS3 client)
+        {
+            this.client = client;
+        }
+
+        public IEnumerable<Location> GetLocations(string bucketName, string fileName)
+        {
+            var response = client.GetObjectAsync(bucketName, fileName).Result;
+            
+            using var streamReader = new StreamReader(response.ResponseStream);
+            var content = streamReader.ReadToEnd();
+
+            return JsonConvert.DeserializeObject<List<Location>>(content);
+        }
+    }
+}
--- GasMon/Program.cs
+++ GasMon/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Internal;
+using Amazon.Runtime;
 
 namespace GasMon
 {
+
     class Program
     {
-        static void Main(string[] args)
+        private const string bucketName = "gasmonitoring-locationss3bucket-pgef0qqmgwba";
+        private const string keyName = "locations.json";
+        // Specify your bucket region (an example region is shown).
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.EUWest2;
+        private static IAmazonS3 client = new AmazonS3Client(bucketRegion);
+
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var locationsFetcher = new LocationsFetcher(client);
+            var locations = locationsFetcher.GetLocations(bucketName, keyName);
+            Console.WriteLine(locations);
         }
     }
 }
