60645ec3c276aa32bdc6340767cfdc6783a45ec8
Jamie Baird <james.baird@softwire.com>
2020-03-31 12:51:50 +0100

    Added queue and deleted queue in main program

--- GasMon/Program.cs
+++ GasMon/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Amazon;
 using Amazon.Internal;
 using Amazon.Runtime;
+using Amazon.SimpleNotificationService.Model;
+using Amazon.SQS;
+using Amazon.SQS.Model;
 
 namespace GasMon
 {
@@ -16,13 +19,26 @@ namespace GasMon
         private const string keyName = "locations.json";
         // Specify your bucket region (an example region is shown).
         private static readonly RegionEndpoint bucketRegion = RegionEndpoint.EUWest2;
-        private static IAmazonS3 client = new AmazonS3Client(bucketRegion);
+        private static IAmazonS3 s3client = new AmazonS3Client(bucketRegion);
+        private static AmazonSQSClient sqsclient = new AmazonSQSClient(bucketRegion);
 
         public static void Main()
         {
-            var locationsFetcher = new LocationsFetcher(client);
-            var locations = locationsFetcher.GetLocations(bucketName, keyName);
-            Console.WriteLine(locations);
+            // var locationsFetcher = new LocationsFetcher(s3client);
+            // var locations = locationsFetcher.GetLocations(bucketName, keyName);
+            // Console.WriteLine(locations);
+            
+            CreateQueueRequest createQueueRequest = new CreateQueueRequest();
+            createQueueRequest.QueueName = "GasMonQueue";
+            CreateQueueResponse createQueueResponse =
+                sqsclient.CreateQueueAsync(createQueueRequest).Result;
+            string queueURL = createQueueResponse.QueueUrl;
+
+            DeleteQueueRequest deleteQueueRequest = new DeleteQueueRequest(queueURL);
+            sqsclient.DeleteQueueAsync(deleteQueueRequest);
+
+
+
         }
     }
 }
